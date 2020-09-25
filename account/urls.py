f25957b12bb0f4fec077a9dbe16b659bd78a4477
Van Step <van.step2186@gmail.com>
2020-09-25 15:06:58 +0400

    first commit

---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
